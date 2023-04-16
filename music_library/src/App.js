ff3598a2d4689a2351a232e1ea46853683608760
unknown <jasmina_t05@outlook.com>
2023-04-16 09:34:55 -0700

    completed first part

---
+++ music_library/src/App.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import SearchBar from './components/SearchBar';
+import Gallery from './components/Gallery';
+
+import './App.css';
+
+function App() {
+  let [search, setSearch] = useState('');
+  let [message, setMessage] = useState('Search for Music!');
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`;
+      const response = await fetch(`https://itunes.apple.com/search?term=${search}`);
+      const resData = await response.json();
+      if (resData.results.length) {
+        setData(resData.results)
+
+      } else {
+        setData([]);
+        setMessage("Nothing found for this artist")
+
+      }
+
+      console.log(resData)
+    }
+
+    fetchData();
+
+  }, [search]);
+
+  return (
+    <div className="App">
+      <SearchBar setSearch={setSearch} />
+      {message}
+
+      <Gallery data={data} />
+
+    </div>
+  );
+}
+
+export default App;
